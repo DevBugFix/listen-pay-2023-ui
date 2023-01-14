ba0fb61f48416422de17927752937286167f8bc9
rao.shahid.work799@gmail.com <rao.shahid.work799@gmail.com>
2023-01-14 12:36:53 +0500

    now event and UI working in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { footerBtn } from './helpers/enums';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,80 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ListenPay "Listen|Earn|Redeem"';
+  public footerBtnRef = footerBtn;
+  public selectedFooterBtn: footerBtn = footerBtn.None;
+
+  onProfileBtn() {
+    if ($('.dashboard').is(":visible")) {
+      $(".btn-sidebar-holder").hide();
+      $(".dashboard").stop(true, true).hide("slide", {
+        direction: "left"
+      }, 200, function () {
+        $(".login").show();
+        $(".wrapper").addClass("loginScreen");
+      });
+    }
+    this.selectedFooterBtn = this.footerBtnRef.Login;
+  }
+
+
+  onAppLink() {
+    console.log("onFooterLink")
+    this.selectedFooterBtn = this.footerBtnRef.App;
+    this.toogleLoginUser();
+  }
+
+  onanalyticsLink() {
+    console.log("onanalyticsLink")
+    this.selectedFooterBtn = this.footerBtnRef.Analytics;
+    this.toogleLoginUser();
+  }
+
+  onUserLink() {
+    console.log("onUserLink()");
+
+    if (this.selectedFooterBtn == this.footerBtnRef.None) {
+      this.selectedFooterBtn = this.footerBtnRef.Login
+    }
+    else if (this.selectedFooterBtn == this.footerBtnRef.User) {
+      this.selectedFooterBtn = this.footerBtnRef.Login
+    } else if (this.selectedFooterBtn == this.footerBtnRef.Login) {
+      this.selectedFooterBtn = this.footerBtnRef.User;
+    }else {
+      this.selectedFooterBtn = this.footerBtnRef.Login;
+    }
+    this.toogleLoginUser();
+  }
+
+  ongiftLink() {
+    console.log("ongiftLink")
+    this.selectedFooterBtn = this.footerBtnRef.Gift;
+    this.toogleLoginUser();
+  }
+
+  onFaqLink() {
+    console.log("onFaqLink")
+    this.selectedFooterBtn = this.footerBtnRef.Faq;
+    this.toogleLoginUser();
+  }
+
+  toogleLoginUser() {
+    if (this.selectedFooterBtn == this.footerBtnRef.Login) {
+      $(".btn-sidebar-holder").hide();
+      $(".dashboard").stop(true, true).hide("slide", {
+        direction: "left"
+      }, 200, function () {
+        $(".login").show();
+        $(".wrapper").addClass("loginScreen");
+      });
+    } else {
+      $(".login").hide();
+      $(".wrapper").removeClass("loginScreen");
+      $(".dashboard").stop(true, true).show("slide", {
+        direction: "left"
+      }, 200, function () {
+        $(".btn-sidebar-holder").show();
+      });
+    }
+  }
 }
